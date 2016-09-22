c3a7470d50191177229184000e7b0bd6aad81071
MonsciergeNetRemote <net.remote@monscierge.com>
2016-09-22 13:38:42 +0530

    Add project files.

---
+++ ReactWebAPIJsonData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReactWebAPIJsonData.Startup))]
+namespace ReactWebAPIJsonData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
